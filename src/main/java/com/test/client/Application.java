62a87a692d01e1f7c647f636f28277f27b234b69
zhengchunfeng <1667368756@qq.com>
2019-12-06 11:50:46 +0800

    Initial commit

---
+++ src/main/java/com/test/client/Application.java
@@ -0,0 +1,19 @@
+package com.test.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengchunfeng
+ * @version 1.0
+ * @className Application
+ * @description
+ * @date 2019/12/5 17:14
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
